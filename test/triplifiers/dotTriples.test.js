1200e9574fb6886030b19b30268eb8740508a5e3
Cristian <cristianvasquez@gmail.com>
2022-04-08 11:01:47 +0200

    index on save

--- test/triplifiers/dotTriples.test.js
+++ test/triplifiers/dotTriples.test.js
@@ -8,27 +8,29 @@ expect.extend({ toMatchSnapshot })
 
 const phrases = [
   'properties :: and values are separated by double colons',
-  'properties and values can be (embedded :: using parenthesis)',
-  'Maria :: ( climbed :: the tree )',
-  'suddenly, (Maria :: (climbed :: the tree)),',
-  'That night,  (Bob :: (ate :: all the pancakes)), and (we :: (are still :: surprised))',
-  'Bob :: (has :: drivers license), (can drive :: true)',
   `
   to simplify things, this
   mini-syntax :: spans
   only one line
   `,
-  'arrays :: [banana, apple]',
-  '[Maria, Bob] :: (climbed :: the tree)',
-  '[Maria, Bob] :: ([climbed,looked at] :: the tree)',
-  '[Maria, Bob] :: ([climbed,looked at] :: [the tree, the sun])',
-  'external link :: <http://example.org>',
+  'Maria :: climbed :: the tree',
+  'subject :: \'Quotes escape :: characters\' :: object',
   'internal link :: [[link]]',
+  'is a :: [[Editor]], [[Jour]]',
+  'external link :: <http://example.org>',
+  'external link :: http://example.org',
   'date :: 2022-01-07 15:07',
-  'vaccination dates :: [2022-01-07, next week]',
-  '(number :: twenty two) (number :: 22)',
+  'arrays :: [banana, apple]',
   'A :: B :: C :: D',
-  'is a :: [[Editor]], [[Jour]]'
+  'vaccination dates :: 2022-01-07, next week',
+  '__THIS__ :: is a :: note',
+  ' something :: points at :: __THIS__',
+
+  // '[Maria, Bob] :: (climbed :: the tree)',
+  // '[Maria, Bob] :: ([climbed,looked at] :: the tree)',
+  // '[Maria, Bob] :: ([climbed,looked at] :: [the tree, the sun])',
+  // 'That night,  (Bob :: (ate :: all the pancakes)), and (we :: (are still :: surprised))',
+  // 'Bob :: (has :: drivers license), (can drive :: true)',
 ]
 
 
